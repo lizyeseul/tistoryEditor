0b65aa2c5092ad445e858685633af6596c16d3d5
liz <lys984prms@naver.com>
2022-10-21 17:57:05 +0900

    init

---
+++ editor/src/main/java/tistory/edit/TistoryEditorApplication.java
@@ -0,0 +1,14 @@
+package tistory.edit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TistoryEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TistoryEditorApplication.class, args);
+		//change
+	}
+
+}
---
+++ editor/src/test/java/tistory/edit/TistoryEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package tistory.edit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TistoryEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
